6148d4d080c1a4a914e13f978e97b78895a6338e
rbhuvan10 <rbhuvan.cs23@bmsce.ac.in>
2025-05-20 19:19:10 +0530

    Update Week 2 FCFS and SJF.c

--- Week 2 FCFS and SJF.c
+++ Week 2 FCFS and SJF.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void sortByArrivalAndBurst(int n, int at[], int bt[], int index[]) {
     for (int i = 0; i < n - 1; i++) {
@@ -7,11 +8,9 @@ void sortByArrivalAndBurst(int n, int at[], int bt[], int index[]) {
                 int temp = at[j];
                 at[j] = at[j + 1];
                 at[j + 1] = temp;
-
                 temp = bt[j];
                 bt[j] = bt[j + 1];
                 bt[j + 1] = temp;
-
                 temp = index[j];
                 index[j] = index[j + 1];
                 index[j + 1] = temp;
@@ -22,40 +21,68 @@ void sortByArrivalAndBurst(int n, int at[], int bt[], int index[]) {
 
 void fcfs(int n, int at[], int bt[], int ct[], int tat[], int wt[]) {
     int time = 0;
-
     for (int i = 0; i < n; i++) {
-        if (time < at[i]) {
-            time = at[i];
-        }
-
+        if (time < at[i]) time = at[i];
         ct[i] = time + bt[i];
         tat[i] = ct[i] - at[i];
         wt[i] = tat[i] - bt[i];
-
         time = ct[i];
     }
 }
 
- void sjf(int n, int at[], int bt[], int ct[], int tat[], int wt[]) {
-    int index[n];
-
-    for (int i = 0; i < n; i++)
-        index[i] = i;
-
-    sortByArrivalAndBurst(n, at, bt, index);
-
-    int time = 0;
-
+void sjfNonPreemptive(int n, int at[], int bt[], int ct[], int tat[], int wt[]) {
+    int remaining[n], completed = 0, current_time = 0;
     for (int i = 0; i < n; i++) {
-        if (time < at[i]) {
-            time = at[i];
+        remaining[i] = bt[i];
+        ct[i] = -1;
+    }
+    while (completed < n) {
+        int shortest = -1, shortest_bt = 999999;
+        for (int i = 0; i < n; i++) {
+            if (at[i] <= current_time && remaining[i] > 0 && remaining[i] < shortest_bt) {
+                shortest = i;
+                shortest_bt = remaining[i];
+            }
         }
+        if (shortest == -1) {
+            current_time++;
+            continue;
+        }
+        ct[shortest] = current_time + remaining[shortest];
+        tat[shortest] = ct[shortest] - at[shortest];
+        wt[shortest] = tat[shortest] - bt[shortest];
+        remaining[shortest] = 0;
+        completed++;
+        current_time = ct[shortest];
+    }
+}
 
-        ct[i] = time + bt[i];
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
-
-        time = ct[i];
+void sjfPreemptive(int n, int at[], int bt[], int ct[], int tat[], int wt[]) {
+    int remaining[n], completed = 0, current_time = 0;
+    for (int i = 0; i < n; i++) {
+        remaining[i] = bt[i];
+        ct[i] = -1;
+    }
+    while (completed < n) {
+        int shortest = -1, shortest_bt = 999999;
+        for (int i = 0; i < n; i++) {
+            if (at[i] <= current_time && remaining[i] > 0 && remaining[i] < shortest_bt) {
+                shortest = i;
+                shortest_bt = remaining[i];
+            }
+        }
+        if (shortest == -1) {
+            current_time++;
+            continue;
+        }
+        remaining[shortest]--;
+        current_time++;
+        if (remaining[shortest] == 0) {
+            ct[shortest] = current_time;
+            tat[shortest] = ct[shortest] - at[shortest];
+            wt[shortest] = tat[shortest] - bt[shortest];
+            completed++;
+        }
     }
 }
 
@@ -67,8 +94,6 @@ void displayResults(int n, int at[], int bt[], int ct[], int tat[], int wt[]) {
         totalTAT += tat[i];
         totalWT += wt[i];
     }
-
-
     float avgTAT = (float)totalTAT / n;
     float avgWT = (float)totalWT / n;
     printf("\nAverage Turnaround Time: %.2f\n", avgTAT);
@@ -78,61 +103,65 @@ void displayResults(int n, int at[], int bt[], int ct[], int tat[], int wt[]) {
 int main() {
     int n;
     int at[100], bt[100], ct[100], tat[100], wt[100];
-
     printf("Enter number of processes: ");
     scanf("%d", &n);
-
     printf("Enter Arrival Time and Burst Time:\n");
-
     for (int i = 0; i < n; i++) {
         printf("Process %d: ", i + 1);
         scanf("%d %d", &at[i], &bt[i]);
     }
 
-
+    for (int i = 0; i < n; i++) ct[i] = tat[i] = wt[i] = 0;
     printf("\nFCFS Scheduling\n");
     fcfs(n, at, bt, ct, tat, wt);
     displayResults(n, at, bt, ct, tat, wt);
 
+    for (int i = 0; i < n; i++) ct[i] = tat[i] = wt[i] = 0;
+    printf("\nSJF Non-Preemptive Scheduling\n");
+    sjfNonPreemptive(n, at, bt, ct, tat, wt);
+    displayResults(n, at, bt, ct, tat, wt);
 
-    for (int i = 0; i < n; i++) {
-        ct[i] = tat[i] = wt[i] = 0;
-    }
-
-
-    printf("\nSJF Scheduling\n");
-    sjf(n, at, bt, ct, tat, wt);
+    for (int i = 0; i < n; i++) ct[i] = tat[i] = wt[i] = 0;
+    printf("\nSJF Preemptive (SRTF) Scheduling\n");
+    sjfPreemptive(n, at, bt, ct, tat, wt);
     displayResults(n, at, bt, ct, tat, wt);
 
     return 0;
 }
 
-OUTPUT:
+Output:
 Enter number of processes: 3
 Enter Arrival Time and Burst Time:
-Process 1: 1 5
-Process 2: 3 4
+Process 1: 1 5 
+Process 2: 3 4 
 Process 3: 0 2
 
 FCFS Scheduling
 
-Process Arrival Burst   Completion      Turnaround      Waiting
-1       1       5       6               5               0
-2       3       4       10              7               3
-3       0       2       12              12              10
+Process	Arrival	Burst	Completion	Turnaround	Waiting
+1	1	5	6		5		0
+2	3	4	10		7		3
+3	0	2	12		12		10
 
 Average Turnaround Time: 8.00
 Average Waiting Time: 4.33
 
-SJF Scheduling
+SJF Non-Preemptive Scheduling
 
-Process Arrival Burst   Completion      Turnaround      Waiting
-1       0       2       2               2               0
-2       1       5       7               6               1
-3       3       4       11              8               4
+Process	Arrival	Burst	Completion	Turnaround	Waiting
+1	1	5	7		6		1
+2	3	4	11		8		4
+3	0	2	2		2		0
 
 Average Turnaround Time: 5.33
 Average Waiting Time: 1.67
 
-Process returned 0 (0x0)   execution time : 10.798 s
-Press any key to continue.
+SJF Preemptive (SRTF) Scheduling
+
+Process	Arrival	Burst	Completion	Turnaround	Waiting
+1	1	5	7		6		1
+2	3	4	11		8		4
+3	0	2	2		2		0
+
+Average Turnaround Time: 5.33
+Average Waiting Time: 1.67
